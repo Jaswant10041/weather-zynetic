bdd08e2b7e1802a314ce7286121c3ac0dd783ef9
Jaswant10041 <jaswanthkumarmadineni@gmail.com>
2025-03-30 04:34:46 +0530

    latest5

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,83 +1,157 @@
 import axios from "axios";
+// const apiKey = import.meta.env.VITE_API_KEY;
 import React, { useEffect, useState } from "react";
 const Weather = () => {
-  const [city,setCity]=useState('');
-  const [queryCity,setQueryCity]=useState('');
-  const [dataUpdated,setDataUpdated]=useState(false);
-  const weatherData = {
-    cityName: null,
-    cityTemperature: null,
-    cityWeatherCondition: null,
-    cityHumidity: null,
-    cityWindSpeed: null,
-    cityWeatherIcon: null,
-    isLoading: null,
-    isError: null,
+  const key='bb60a37ba6d01f853b6bcd5f5899b110';
+  const [city, setCity] = useState("");
+  const [queryCity, setQueryCity] = useState("");
+  const [history, setHistory] = useState([]);
+  const [error, setError] = useState("");
+  const initialValues = {
+    cityName: "N/A",
+    cityTemperature: "N/A",
+    cityWeatherCondition: "N/A",
+    cityHumidity: "N/A",
+    cityWindSpeed: "N/A",
+    cityWeatherIcon: "N/A",
   };
-  function handleSearch(){
-    if(city===''){
-      console.log("Enter valid City name");
-      return ;
+  const [weatherData, setWeatherData] = useState(initialValues);
+
+  function handleSearch(cityName) {
+    if (cityName === "") {
+      setError("Enter valid city name");
+      return;
     }
-    setTimeout(()=>{
-      setQueryCity(city);
-    },2000);
+    // setTimeout(()=>{
+    setQueryCity(cityName);
+    // },2000);
   }
   async function fetchWeatherDetails() {
     try {
-      weatherData.isLoading = true;
       const response = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${queryCity}&appid=bb60a37ba6d01f853b6bcd5f5899b110&units=metric`
+        `https://api.openweathermap.org/data/2.5/weather?q=${queryCity}&appid=${key}&units=metric`
       );
       const { data } = response;
       console.log(data);
       if (!data) {
         throw new Error("Data should not empty");
       }
-      weatherData.cityName = data?.name;
-      weatherData.cityTemperature = data?.main?.temp;
-      weatherData.cityHumidity = data?.main?.humidity;
-      weatherData.cityWindSpeed = data?.wind?.speed;
-      weatherData.cityWeatherCondition = data?.weather[0]?.main;
-      weatherData.cityWeatherIcon = data?.weather[0]?.icon;
-      weatherData.isLoading = false;
-      weatherData.isError = false;
-      // setDataUpdated(true);
-      console.log(weatherData);
+      setError("");
+      const freshWeatherData = {};
+      freshWeatherData.cityName = data?.name;
+      freshWeatherData.cityTemperature = data?.main?.temp;
+      freshWeatherData.cityHumidity = data?.main?.humidity;
+      freshWeatherData.cityWindSpeed = data?.wind?.speed;
+      freshWeatherData.cityWeatherCondition = data?.weather[0]?.main;
+      freshWeatherData.cityWeatherIcon = data?.weather[0]?.icon;
+      console.log(freshWeatherData);
+      setWeatherData(freshWeatherData);
+      if (history.length < 6) {
+        setHistory((prev) => [freshWeatherData.cityName, ...prev]);
+      } else {
+        history.remove(history[history.length - 1]);
+        setHistory((prev) => [freshWeatherData.cityName, ...prev]);
+      }
+      //pune
+      //chennai
+      //delhi
+      //bengaluru
+      //hyderabad
+      //punjab
+      //anantapur
     } catch (err) {
+      const { status } = err;
+      if (status === 404) {
+        // alert("City Not found Enter valid City Name")
+        setError("City Not found Enter valid City Name");
+      }
       console.log(err);
     }
   }
   useEffect(() => {
-    if(queryCity===''){
-      return ;
+    if (queryCity === "") {
+      return;
     }
     fetchWeatherDetails();
   }, [queryCity]);
   return (
     <div className="m-3">
-      <div className="flex justify-center">
-        <input type="text" placeholder=" Search " className="border-2 border-indigo-600 w-xs rounded-md" onChange={(e)=>{
-          setCity(e.target.value);
-        }} value={city}/>
-        <button onClick={handleSearch}>Search</button>
+      <div className="flex justify-center mt-10">
+        <form className="flex" onSubmit={(e)=>e.preventDefault()}>
+          <input
+            type="text"
+            placeholder=" Search "
+            className="border-2 border-indigo-600 w-md rounded-md p-2"
+            onChange={(e) => {
+              setCity(e.target.value);
+            }}
+            value={city}
+            required
+          />
+          <div className="pl-1">
+            <button
+              onClick={() => handleSearch(city)}
+              className="border-2 border-emerald-400 p-2 rounded-md bg-gray-200"
+            >
+              Search
+            </button>
+          </div>
+        </form>
       </div>
-      <div>
-        {
-          weatherData?.cityName
-        }
-        {
-          weatherData?.cityTemperature
-        }
-        {
-          weatherData?.cityWeatherCondition
-        }
-        {
-          weatherData?.cityWindSpeed
-        }
-        {
-          weatherData?.cityWeatherIcon
-        }
+      <div className="flex justify-around mt-15 pt-15">
+        {error === "" && (
+          <div className="flex flex-row text-center font-medium text-3xl mx-3">
+            <div className="flex flex-col p-2 mx-3 bg-gray-200 rounded-md justify-center h-45">
+              <label>City Name </label>
+              <p>{weatherData?.cityName}</p>
+            </div>
+            <div className="flex flex-col p-2 bg-gray-200 mx-3 rounded-md justify-center">
+              <label>Temperature </label>
+              <p>{weatherData?.cityTemperature} °C</p>
+            </div>
+            <div className="flex flex-col p-2 bg-gray-200 mx-3 rounded-md justify-center">
+              <label>Weather Condition </label>
+              <p>{weatherData?.cityWeatherCondition}</p>
+            </div>
+            <div className="flex flex-col p-2 bg-gray-200 mx-3 rounded-md justify-center">
+              <label>Humidity </label>
+              <p>{weatherData?.cityHumidity}%</p>
+            </div>
+            <div className="flex flex-col p-2 bg-gray-200 mx-3 rounded-md justify-center">
+              <label>Wind Speed </label>
+              <p>{weatherData?.cityWindSpeed} m/s</p>
+            </div>
+            <div className="flex flex-col p-2 bg-gray-200 mx-2 rounded-md justify-center">
+              <label>Weather Icon </label>
+              <p>{weatherData?.cityWeatherIcon}</p>
+            </div>
+          </div>
+        )}
+        {<div className="font-medium text-2xl">{error}</div>}
+        <div className="flex flex-col text-center h-45 w-40">
+          <div className="font-medium text-2xl p-2 bg-gray-200 justify-center rounded-md">
+            History
+          </div>
+          <ul>
+            {history.length === 0 && (
+              <li className="font-md text-2xl bg-gray-200 p-2">No recent search</li>
+            )}
+            {history.map((item, index) => {
+              if (index === 0) {
+                return;
+              }
+              return (
+                <li
+                  key={index}
+                  onClick={() => handleSearch(item)}
+                  className="font-md text-2xl p-2 bg-gray-200"
+                >
+                  {item}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </div>
   );
