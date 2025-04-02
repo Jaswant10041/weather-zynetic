91a65108d1d334a953fb0297ff4ae1ce95c32866
Jaswant10041 <jaswanthkumarmadineni@gmail.com>
2025-04-02 14:46:00 +0530

    latest20

--- src/Components/WeatherDetails.jsx
+++ src/Components/WeatherDetails.jsx
@@ -4,28 +4,28 @@ const WeatherDetails = (props) => {
   const { weatherData } = props;
   return (
     <div>
-      <div className="sm:flex flex-row text-center font-medium sm:text-3xl mx-auto mt-15 pt-15">
-        <div className="flex flex-col p-2 sm:p-8 bg-gray-200 mx-auto rounded-md justify-center ml-3 mt-1 dark:bg-gray-700 dark:text-white">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 text-center font-medium sm:text-3xl mx-auto mt-15 pt-15 gap-2">
+        <div className="bg-white dark:bg-gray-700 p-4 rounded-lg text-center">
           <label className="dark:text-gray-300">City Name</label>
           <p className="pt-1">{weatherData?.cityName}</p>
         </div>
-        <div className="flex flex-col p-2 sm:p-8 bg-gray-200 mx-auto rounded-md justify-center ml-3 mt-1 dark:bg-gray-700 dark:text-white">
+        <div className="bg-white dark:bg-gray-700 p-4 rounded-lg text-center">
           <label className="dark:text-gray-300">Temperature</label>
           <p className="pt-1">{weatherData?.cityTemperature} °C</p>
         </div>
-        <div className="flex flex-col p-2 sm:p-8 bg-gray-200 mx-auto rounded-md justify-center ml-3 mt-1 dark:bg-gray-700 dark:text-white">
+        <div className="bg-white dark:bg-gray-700 p-4 rounded-lg text-center">
           <label className="dark:text-gray-300">Weather Condition</label>
           <p className="pt-1">{weatherData?.cityWeatherCondition}</p>
         </div>
-        <div className="flex flex-col p-2 sm:p-8 bg-gray-200 mx-auto rounded-md justify-center ml-3 mt-1 dark:bg-gray-700 dark:text-white">
+        <div className="bg-white dark:bg-gray-700 p-4 rounded-lg text-center">
           <label className="dark:text-gray-300">Humidity</label>
           <p className="pt-1">{weatherData?.cityHumidity} %</p>
         </div>
-        <div className="flex flex-col p-2 bg-gray-200 mx-auto rounded-md justify-center ml-3 mt-1 dark:bg-gray-700 dark:text-white">
+        <div className="bg-white dark:bg-gray-700 p-4 rounded-lg text-center">
           <label className="dark:text-gray-300">Wind Speed</label>
           <p className="pt-1">{weatherData?.cityWindSpeed} m/s</p>
         </div>
-        <div className="flex flex-col p-2 bg-gray-200 mx-auto rounded-md justify-center ml-3 mt-1 dark:bg-gray-700 dark:text-white">
+        <div className="bg-white dark:bg-gray-700 p-4 rounded-lg text-center">
           <label className="dark:text-gray-300">Weather Icon</label>
           {weatherData.cityWeatherIcon !== "N/A" ? (
             <div className="flex justify-center ">
